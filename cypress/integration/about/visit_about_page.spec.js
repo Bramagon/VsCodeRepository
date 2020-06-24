22ba6b98139d237b550664c6fd6976f68751dd87
Bram Douven <I346784@fhict.nl>
2020-06-24 10:01:38 +0200

    Increased text length

--- cypress/integration/about/visit_about_page.spec.js
+++ cypress/integration/about/visit_about_page.spec.js
@@ -2,6 +2,7 @@ describe("about test", () => {
     it("should visit about page", () => {
         cy.visit("/");
         cy.get('[routerlink="/about"]').click();
-
+        cy.get('.title').should('contain', 'about');
+        cy.get('.paragraph').should('contain', 'Welcome to my website!');
     });
 });
--- cypress/integration/homepage/visit_home_page.spec.js
+++ cypress/integration/homepage/visit_home_page.spec.js
@@ -1,6 +1,6 @@
 describe("home test", () => {
     it("should visit home page", () => {
         cy.visit("/");
-
+        cy.get('#title').should('contain', 'Welcome to AngularGame');
     });
 });
